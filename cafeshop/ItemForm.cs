db1933448b170d857d7560f6e69c60961ba8d73b
Aida Mortazavi <amorta8181@gmail.com>
2024-04-09 09:37:56 -0700

    make order

--- cafeshop/ItemForm.cs
+++ cafeshop/ItemForm.cs
@@ -14,5 +14,15 @@ namespace cafeshop
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
